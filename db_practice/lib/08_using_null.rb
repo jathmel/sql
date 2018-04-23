914f756450c39259242234b9bb31a35582d94fe8
Jathmel <jathmel@gmail.com>
2018-04-23 15:52:09 -0400

    add more code

--- db_practice/lib/08_using_null.rb
+++ db_practice/lib/08_using_null.rb
@@ -59,6 +59,10 @@ def teachers_and_mobiles
   # 444 2266' if no number is given. Show teacher name and mobile
   # #number or '07986 444 2266'
   execute(<<-SQL)
+  SELECT
+    name, COALESCE(mobile, '07986 444 2266')
+  FROM
+    teachers
   SQL
 end
 
@@ -67,6 +71,12 @@ def teachers_and_depts
   # department name. Use the string 'None' where there is no
   # department.
   execute(<<-SQL)
+  SELECT
+    teachers.name, COALESCE(depts.name, 'None')
+  FROM
+    teachers
+  LEFT JOIN
+      depts ON teachers.dept_id = depts.id
   SQL
 end
 
@@ -75,6 +85,10 @@ def num_teachers_and_mobiles
   # mobile phones.
   # NB: COUNT only counts non-NULL values.
   execute(<<-SQL)
+  SELECT
+   COUNT(name), COUNT(mobile)
+  FROM
+    teachers
   SQL
 end
 
@@ -83,6 +97,16 @@ def dept_staff_counts
   # the number of staff. Structure your JOIN to ensure that the
   # Engineering department is listed.
   execute(<<-SQL)
+  SELECT
+    depts.name, COUNT(teachers.name)
+  FROM
+    depts
+  LEFT JOIN
+    teachers ON depts.id = teachers.dept_id
+  GROUP BY
+    depts.name
+
+
   SQL
 end
 
@@ -90,6 +114,18 @@ def teachers_and_divisions
   # Use CASE to show the name of each teacher followed by 'Sci' if
   # the the teacher is in dept 1 or 2 and 'Art' otherwise.
   execute(<<-SQL)
+  SELECT
+    teachers.name,
+  CASE
+  WHEN
+    teachers.dept_id IN (1, 2)
+  THEN
+    'Sci'
+  ELSE
+    'Art'
+  END
+  FROM
+    teachers
   SQL
 end
 
@@ -98,5 +134,21 @@ def teachers_and_divisions_two
   # the teacher is in dept 1 or 2, 'Art' if the dept is 3, and
   # 'None' otherwise.
   execute(<<-SQL)
+  SELECT
+    teachers.name,
+  CASE
+  WHEN
+    teachers.dept_id IN (1, 2)
+  THEN
+    'Sci'
+  WHEN
+    teachers.dept_id = 3
+  THEN
+    'Art'
+  ELSE
+    'None'
+  END
+  FROM
+    teachers
   SQL
 end
